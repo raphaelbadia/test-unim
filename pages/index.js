be5f52de28fb36341f9c9ff154236927f9ee60ed
Raphaël Badia <raphael@badia.cc>
2021-08-04 12:25:03 +0200

    wow

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <form action="https://www.staging.maiia.com/api-unim-test" method="POST" target="_blank">
-        <input type="text" id="unim" name="unim" style={{display: 'none'}}/>
+        <input type="text" id="unim" name="unim" value="success" style={{display: 'none'}}/>
         <input type="submit" value="Voir sur maiia"/>
       </form>
     </div>
